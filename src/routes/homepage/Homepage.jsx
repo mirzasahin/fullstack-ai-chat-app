ca52fe76bfb107a32d7deee323723fd8e44452a3
Mirza Sahin <salihmirza123@gmail.com>
2024-07-31 20:35:55 +0300

    Implement react typing animation

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import './homepage.css'
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react'
 
 const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
   return (
     <div className='homepage'>
       <img src="/orbital.png" alt="" className='orbital' />
@@ -23,6 +26,34 @@ const Homepage = () => {
             <div className="bg"></div>
           </div>
           <img src="/bot.png" alt="" className='bot' />
+          <div className='chat'>
+            <img src={typingStatus === "human1" ? "/human1.jpeg" : typingStatus === "human2" ? "/human2.jpeg" : "/bot.png" } alt="" />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Human: We produce food for Mice',
+                2000, () => {
+                  setTypingStatus("bot")
+                }, // wait 1s before replacing "Mice" with "Hamsters"
+                'Bot: We produce food for Hamsters',
+                2000, () => {
+                  setTypingStatus("human2")
+                },
+                'Human2: We produce food for Turkish Cows',
+                2000, () => {
+                  setTypingStatus("bot")
+                },
+                'Bot: We produce food for Chinchillas',
+                2000, () => {
+                  setTypingStatus("human1")
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
         </div>
 
       </div>

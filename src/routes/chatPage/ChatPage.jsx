a5348c8b6448981c129a6c957b753ecc4f190401
Mirza Sahin <salihmirza123@gmail.com>
2024-08-03 21:16:40 +0300

    Add scroll down animation when refresh the chat page

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,8 +1,97 @@
+import { useEffect, useRef } from 'react'
 import './chatPage.css'
 
 const ChatPage = () => {
+
+  const endRef = useRef(null)
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" })
+  }, [])
   return (
-    <div className='chatPage'>ChatPage</div>
+    <div className='chatPage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto natus aspernatur ratione doloremque error dolorem mollitia assumenda maxime, deserunt deleniti! Optio cum eos voluptatem quibusdam exercitationem repellat sint, animi iure!</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div><div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div><div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div><div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div><div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div ref={endRef} />
+        </div>
+      </div>
+    </div>
   )
 }
 

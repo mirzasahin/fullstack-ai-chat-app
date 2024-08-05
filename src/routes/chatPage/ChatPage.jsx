408d83451569e47a81e429aa8d7866fa4bf3afca
Mirza Sahin <salihmirza123@gmail.com>
2024-08-05 07:55:59 +0300

    Add new prompt section & update style

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,13 +1,7 @@
-import { useEffect, useRef } from 'react'
 import './chatPage.css'
+import NewPrompt from '../../components/newPrompt/NewPrompt'
 
 const ChatPage = () => {
-
-  const endRef = useRef(null)
-
-  useEffect(() => {
-    endRef.current.scrollIntoView({ behavior: "smooth" })
-  }, [])
   return (
     <div className='chatPage'>
       <div className="wrapper">
@@ -88,7 +82,7 @@ const ChatPage = () => {
           <div className="message user">Test message from user</div>
           <div className="message">Test message</div>
           <div className="message user">Test message from user</div>
-          <div ref={endRef} />
+          <NewPrompt />
         </div>
       </div>
     </div>

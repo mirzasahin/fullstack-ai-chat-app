6e907f1f0c461733ddaad575aec69cb33e00e57d
Mirza Sahin <salihmirza123@gmail.com>
2024-08-10 14:03:43 +0300

    Implement Google Gemini AI API

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -2,8 +2,13 @@ import { useEffect, useRef, useState } from 'react'
 import './newPrompt.css'
 import Upload from '../upload/Upload'
 import { IKImage } from 'imagekitio-react'
+import model from '../../lib/gemini.js'
+import Markdown from "react-markdown"
 
 const NewPrompt = () => {
+
+  const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState("")
 
   const [img, setImg] = useState({
     isLoading: false,
@@ -14,24 +19,43 @@ const NewPrompt = () => {
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" })
-  }, [])
-  
+  }, [question, answer, img.dbData])
+
+  // AI PROMPT FUNCTIONALITY CODE
+  const add = async (text) => {
+    setQuestion(text)
+
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text())
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text)
+  }
+
   return (
     <>
-    {img.isLoading && <div>Loading...</div>}
-    {img.dbData?.filePath && (
-      <IKImage
-        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-        path={img.dbData?.filePath}
-        width={280}
-        transformation={[{ width: 380 }]}
-      />
-    )}
-    <div className='endChat' ref={endRef}></div>
-      <form className='newForm' action="">
-       <Upload setImg={setImg} />
+      {img.isLoading && <div>Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width={280}
+          transformation={[{ width: 380 }]}
+        />
+      )}
+      {question && <div className='message user'>{question}</div>}
+      {answer && <div className='message'><Markdown>{answer}</Markdown></div>}
+      <div className='endChat' ref={endRef}></div>
+      <form className='newForm' onSubmit={handleSubmit}>
+        <Upload setImg={setImg} />
         <input id='file' type="file" multiple={false} hidden />
-        <input type="text" placeholder='Ask anything...' />
+        <input type="text" name='text' placeholder='Ask anything...' />
         <button>
           <img src="/arrow.png" alt="" />
         </button>
